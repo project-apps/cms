3cc6e16737775e3e2565aebe5a9e286adafc8dfc
mishrarajan <mishra.k.rajan@gmail.com>
2020-06-06 09:16:27 +0530

    env variables used.

--- reader/awsBucketReader.js
+++ reader/awsBucketReader.js
@@ -1,9 +1,31 @@
 'use strict'
+const logger = require('../util/log4js.js');
 var AWS = require('aws-sdk');
-AWS.config.update({
-  accessKeyId: "AKIA5CYUVKDYFDTPYAEA",
-  secretAccessKey: "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"
-});
+/*if (process.env.AWS_Access_key_ID && process.env.AWS_Secret_access_key) {
+  const accessKeyId = process.env.AWS_Access_key_ID;
+  const secretAccessKey = process.env.AWS_Secret_access_key;
+  AWS.config.update({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
+  });
+}*/
+if (process.env.VCAP_SERVICES) {
+  var env = JSON.parse (process.env.VCAP_SERVICES);
+  const accessKeyId = env.AWS_Access_key_ID;
+  const secretAccessKey = env.AWS_Secret_access_key;
+  AWS.config.update({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
+  });
+}
+else{
+  var msg = "Unable to configure AWS security keys from VCAP_SERVICES.";
+  logger.error(msg);
+  throw new Error(msg);
+}
+//const accessKeyId = process.env.accessKeyId;;
+//const secretAccessKey = process.env.secretAccessKey;;
+
 var s3 = new AWS.S3();
 exports.listObjects = (params, path, cb)=>{
   try{
